679053f63e852e68a0e2a329bd253430b2453439
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-02-25 23:31:19 +0900

    add basic function

--- app/src/main/java/com/github/bassaer/simplemvp/MainActivity.kt
+++ app/src/main/java/com/github/bassaer/simplemvp/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.run {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add( R.id.contentFrame, CounterFragment())
+        transaction.commit()
     }
 }
